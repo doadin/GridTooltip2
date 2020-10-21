5a9e4b408690009b3adb6392f0b77b7245121c40
tbkizle <tbkizle@gmail.com>
2020-10-20 22:50:32 -0400

    Tabs to Spaces and Cleanup

--- GridTooltip2.lua
+++ GridTooltip2.lua
@@ -9,13 +9,13 @@ GridTooltip2.defaultDB = {
 }
 
 GridTooltip2.options = {
-	name = "GridTooltip2",
-	desc = "Options for GridTooltip2.",
-	order = 2,
-	type = "group",
-	childGroups = "tab",
-	disabled = InCombatLockdown,
-	args = {
+    name = "GridTooltip2",
+    desc = "Options for GridTooltip2.",
+    order = 2,
+    type = "group",
+    childGroups = "tab",
+    disabled = InCombatLockdown,
+    args = {
     }
 }
 
@@ -26,7 +26,7 @@ local function FindTooltip(unit, texture, index)
     local i = 0
     --search from the last index the texture was found to the left and right for the texture
     local name, icon, count, debuffType, duration, expirationTime, unitCaster, isStealable, _, spellId = UnitDebuff(unit, index)
-    while name or index - i > 1 do 
+    while name or index - i > 1 do
         if icon == texture then
             return index + i, spellId
         end
@@ -37,25 +37,25 @@ local function FindTooltip(unit, texture, index)
         end
         name, icon, count, debuffType, duration, expirationTime, unitCaster, isStealable, _, spellId = UnitDebuff(unit, index + i)
     end
-    
+
     return nil
 end
 
 function GridTooltip2.SetIndicator(frame, indicator, color, text, value, maxValue, texture, start, duration, stack)
-	
-	if texture and GridTooltip2.db.profile.enabledIndicators[indicator] then
+
+    if texture and GridTooltip2.db.profile.enabledIndicators[indicator] then
         if frame.unit and UnitExists(frame.unit)then
             frame.ToolTip = texture
             if lastMouseOverFrame then
                 GridTooltip2.OnEnter(lastMouseOverFrame)
-            end            
+            end
         end
-	end
+    end
 end
 
 
 
-function GridTooltip2.ClearIndicator(frame, indicator)   
+function GridTooltip2.ClearIndicator(frame, indicator)
     if GridTooltip2.db.profile.enabledIndicators[indicator] then
         frame.ToolTip = nil
         frame.ToolTipIndex = nil
@@ -63,24 +63,23 @@ function GridTooltip2.ClearIndicator(frame, indicator)
 end
 
 function GridTooltip2.CreateFrames(gridFrameObj, frame)
-    local f = frame
     frame:HookScript("OnEnter", GridTooltip2.OnEnter)
-	frame:HookScript("OnLeave", GridTooltip2.OnLeave)
+    frame:HookScript("OnLeave", GridTooltip2.OnLeave)
 end
 
 function GridTooltip2.OnEnter(frame)
     local unitid = frame.unit
-    if not unitid then return end   
+    if not unitid then return end
     lastMouseOverFrame = frame
-    
-    if not frame.ToolTip then return end 
-    
+
+    if not frame.ToolTip then return end
+
     local id
     frame.ToolTipIndex, id = FindTooltip(unitid, frame.ToolTip, frame.ToolTipIndex)
 
     if frame.ToolTipIndex then
         GameTooltip_SetDefaultAnchor(GameTooltip, UIParent)
-        GameTooltip:SetUnitDebuff(unitid, frame.ToolTipIndex)        
+        GameTooltip:SetUnitDebuff(unitid, frame.ToolTipIndex)
         GameTooltip:Show()
     end
 end
@@ -94,21 +93,21 @@ end
 
 function GridTooltip2:OnInitialize()
     if not self.db then
-		self.db = Grid.db:RegisterNamespace(self.moduleName, { profile = self.defaultDB or { } })
-	end
-    
+        self.db = Grid.db:RegisterNamespace(self.moduleName, { profile = self.defaultDB or { } })
+    end
+
     GridTooltip2.knownIndicators = {}
-    
+
     GridFrame:RegisterIndicator("tooltip", "Tooltip dummy. Do not use!",
         function(frame)
             GridTooltip2.CreateFrames(nil, frame)
             return {}
         end,
-        
+
         function(self)
             local indicators = self.__owner.indicators
             for id, indicator in pairs(indicators) do
-                if not GridTooltip2.knownIndicators[id] then 
+                if not GridTooltip2.knownIndicators[id] then
                     GridTooltip2.options.args[id] = {
                         name = id,
                         desc = "Display tooltip for indicator: "..GridFrame.indicators[id].name,
@@ -125,7 +124,7 @@ function GridTooltip2:OnInitialize()
                 end
             end
         end,
-        
+
         function()
         end,
         function()
